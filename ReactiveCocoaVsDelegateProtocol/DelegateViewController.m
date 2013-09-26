b73f35244cd62f4f68eb878f01332b719faef2e9
Christopher Motl <cmotl@cmotl.com>
2013-09-25 20:26:58 -0500

    Added all needed views

---
+++ ReactiveCocoaVsDelegateProtocol/DelegateViewController.m
@@ -0,0 +1,29 @@
+//
+//  SecondViewController.m
+//  ReactiveCocoaVsDelegateProtocol
+//
+//  Created by Christopher Motl on 9/25/13.
+//  Copyright (c) 2013 Christopher Motl. All rights reserved.
+//
+
+#import "DelegateViewController.h"
+
+@interface DelegateViewController ()
+
+@end
+
+@implementation DelegateViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ ReactiveCocoaVsDelegateProtocol/ReactiveViewController.m
@@ -0,0 +1,29 @@
+//
+//  FirstViewController.m
+//  ReactiveCocoaVsDelegateProtocol
+//
+//  Created by Christopher Motl on 9/25/13.
+//  Copyright (c) 2013 Christopher Motl. All rights reserved.
+//
+
+#import "ReactiveViewController.h"
+
+@interface ReactiveViewController ()
+
+@end
+
+@implementation ReactiveViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
